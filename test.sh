fdcaf8dedbff1acb550c15f1ad824ec34beb8ab1
Chi Vinh Le <vinh@wikiwi.io>
2016-07-12 12:01:31 +0700

    Improve testing

--- test.sh
+++ test.sh
@@ -1,51 +1,60 @@
 #!/bin/bash
 set -eux
 
-PORT=${PORT:-9000}
-TAG=${TAG:-test}
+IMAGE=${IMAGE:-wikiwi/s3-bucket-creator:test}
+NETWORK=${NETWORK:-wikiwi_isolated_nw}
 
 cleanup() {
   minioCID=${minioCID:-''}
   if [ -n "$minioCID" ]; then
     echo Stopping Minio Server...
     docker stop $minioCID
+    docker rm $minioCID
   fi
 
   cid=${cid:-''}
   if [ -n "$cid" ]; then
     echo Stopping long runnign container...
     docker stop $cid
+    docker rm $cid
   fi
+
+  docker network rm ${NETWORK} || true
 }
 trap cleanup EXIT
 
 # Build container.
-docker build -t s3-bucket-creator:${TAG} .
+docker build -t ${IMAGE} .
+
+# Create network.
+docker network create ${NETWORK}
 
 # Setup minio as S3 Test Server.
 docker pull minio/minio
 minioCID=$(docker run -e MINIO_ACCESS_KEY=ACCESSKEY \
            -e MINIO_SECRET_KEY=SECRETKEY \
-           -p 127.0.0.1:${PORT}:${PORT} -d \
+           --net ${NETWORK} -d \
+           --name minio \
            minio/minio /export)
 
 # Create first bucket.
 docker run -e S3_BUCKET_NAME=my-bucket \
-           -e S3_ENDPOINT=http://127.0.0.1:${PORT} \
+           -e S3_ENDPOINT=http://minio:9000 \
            -e S3_ACCESS_KEY=ACCESSKEY \
            -e S3_SECRET_KEY=SECRETKEY \
            -e INITIAL_DELAY=2 \
-           --net host \
-           s3-bucket-creator:${TAG}
+           --net ${NETWORK} \
+           --rm \
+           ${IMAGE}
 
 # Create second bucket and sleep infinitely.
 cid=$(docker run -e S3_BUCKET_NAME=my-second-bucket \
-           -e S3_ENDPOINT=http://127.0.0.1:${PORT} \
+           -e S3_ENDPOINT=http://minio:9000 \
            -e S3_ACCESS_KEY=ACCESSKEY \
            -e S3_SECRET_KEY=SECRETKEY \
            -e INFINITE_SLEEP=true \
-           --net host -d \
-           s3-bucket-creator:${TAG})
+           --net ${NETWORK} -d \
+           ${IMAGE})
 
 sleep 10
 
@@ -54,5 +63,4 @@ docker exec ${cid} mc ls s3host/my-bucket
 docker exec ${cid} mc ls s3host/my-second-bucket
 docker exec ${cid} mc rm s3host/my-bucket
 docker exec ${cid} mc rm s3host/my-second-bucket
-
 
